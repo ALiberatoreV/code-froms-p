ef8f94596b0c7e97c775afe48fbb1516f5126b2d
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-11-28 21:19:17 -0500

    2

--- forms.js
+++ forms.js
@@ -1,16 +1,26 @@
-let list = thelist();
-const sectionTag = document.querySelector("#section");
-
+let list = "";
+const sectionTag = document.querySelector("section");
+const listI = document.querySelector("#listItem").value;
 
 
 document.querySelector(`form`).addEventListener("submit", function(event) {
     event.preventDefault();
 
-    list = document.querySelector("#listItem").value;
-    
+    list = listI.value;
+
+
+let template = 
+`
+    <h2>list: ${listI} </h2>
+
+`
+
 
 })
-const template = list.map(list => `
-    <h2>List: ${list} </h2>
-`);
+sectionTag.innerHTML = template.map(list);
+// const template = list.map(list => `
+//     <h2>List: ${list} </h2>
+// `);
+
+
 
